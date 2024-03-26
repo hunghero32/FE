42a19288ef44b49762efdd025bc03d5e8f288cde
hunghero32 <hung87800@gmail.com>
2024-03-26 17:22:20 +0700

    BUỔI 9

---
+++ Buoi9/supplier.js
@@ -0,0 +1,9 @@
+[
+    {"id":1 , "name": "Lenovo"},
+    {"id":2 , "name": "Dell"},
+    {"id":3 , "name": "Samsung"},
+    {"id":4 , "name": "Surface"},
+    {"id":5 , "name": "Acer"},
+    {"id":6 , "name": "Asus"},
+    {"id":7 , "name": "MSI"}
+]
